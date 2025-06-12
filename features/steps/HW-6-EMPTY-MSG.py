2f23cde882ff71a6043615ebc07acc001b8a75ce
Lubna1k <lubnamansoorkhan@gmail.com>
2025-06-11 22:49:32 -0400

    features/steps/HW-6-EMPTY-MSG.py

--- features/steps/HW-6-EMPTY-MSG.py
+++ features/steps/HW-6-EMPTY-MSG.py
@@ -2,22 +2,16 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
-####
-
-
 
 @given('Open target main site')
 def open_target_main(context):
     context.driver.get("https://www.target.com/")
 
 
-
-
-
 @when('Click on cart icon')
 def click_sign_in(context):
     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartIcon']").click()
-    sleep(5)
+    sleep(3)
 
 @then('Verify Your cart is "empty” message is shown')
 def verify_cart_is_empty(context):
