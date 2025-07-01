e2621823fc67814e710294b55fba5086bb926e6d
Lubna1k <lubnamansoorkhan@gmail.com>
2025-06-30 23:47:41 -0400

    features/steps/HW-4-CSS.py

--- features/steps/HW-4-CSS.py
+++ features/steps/HW-4-CSS.py
@@ -14,49 +14,17 @@ def step_impl(context):
     context.driver.get("https://www.target.com/circle")
 
 
+
 @then("Verify page is correct")
 def verify_page(context):
     context.driver.get("https://www.target.com/circle")
 
 
-@when("user click on circle btn")
-def Verify_click_circle_btn(context):
-    context.driver.find_element(By.XPATH, "https://www.target.com/circle,")
-
-# Verify Links-LUBNA
-#@given('target circle page')
-#def open_target(context):
-   # context.driver.get('https://www.target.com/circle')
-    #context.driver.get('https://www.target.com/l/target-circle/-/N-pzno9')
-
-
-#@when('Verify {links_here}')
-#def verify_links_here(context, links_here):
-  ##  links = context.driver.find_elements(*CIRCLE_LINKS)
-   # assert len(links) == int(links_here), f"Expected {links_here} links, got {len(links_here)}"
-
-
-
-# @when('find {find_links}')
-# def verify_links(context, find_links):
-#     context.driver.find_element(*SEARCH_FIELD).send_keys(find_links)
-#     context.driver.find_element(*SEARCH_BUTTON).click()
-#     sleep(2)
-#
-# @then('Verify page has {10_benefit_cell}')
-# def Verify_page_10_cell(context, cell):
-#     context.driver.find_element("cell-item-content")
-#     assert len("cell-item-content")
+@when('user click on circle btn')
+def click_0n_circle(context):
+    context.driver.find_element(By.XPATH, "//div[@class='cell-item-content']").click()
+    sleep(3)
 
 
 
 
-# @when('Add to cart')
-# def add_to_cart(context):
-#     context.driver.find_element(By.CSS_SELECTOR, "[data-test='orderPickupButton'][id*='addToCartButton']").click()
-#     sleep(2)
-#     context.driver.find_element(By.CSS_SELECTOR, "[data-test*='orderPickupButton']").click()
-#     sleep(2)
-#     context.driver.find_element(By.XPATH, "//a[text()='View cart & check out']").click()
-#     sleep(2)
-#     context.driver.find_element(By.CSS_SELECTOR, "[data-test='cart-order-summary']").text('total')
--- features/tests/HW-4-CSS.feature
+++ features/tests/HW-4-CSS.feature
@@ -7,18 +7,6 @@ Scenario: Verify Target Circle benefits
     Then Verify page is correct
     When user click on circle btn
 
-Scenario: Page is opening
-   #Given I should see at least 10 benefit cells
-  #  Then  Verify page is open
-   # Then I should see at least 10 benefit cells
-
-#Scenario: the Target Circle page
-#  Given target circle page
-# # When Verify links here
-##  When find for links
-##  Then Verify page has 10 benefit cell
-
-
   #lubna creat
   #search 10 benefit cells
 
